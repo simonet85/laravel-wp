ee3d2a90f4b7acd84222f46148318485f09db4b8
simonet85 <christian.85@live.fr>
2020-10-26 11:38:28 +0100

    alter postsTableSeeder & add column publised_at to post table

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Faker\Factory;
 use Illuminate\Database\Seeder;
 
@@ -12,29 +13,37 @@ class PostsTableSeeder extends Seeder
      */
     public function run()
     {
-        //Resetting the table column
-        DB::table('posts')->truncate();
+         // reset the posts table
+         DB::table('posts')->truncate();
 
-        //Adding posts to the posts table
-        $posts = [];
-        $faker = Factory::create();
-        for ($i=1; $i <= 10; $i++) { 
-            $image = 'Post_Image_'.rand(1,5).'_thumb'.'.jpg';
-            $date = date('Y-m-d H:i:s', strtotime("2020-10-25 19:45:00 +{$i} days"));
-            $posts[] = [
-                'author_id'=> rand(1, 3),
-                'title'=> $faker->sentence( rand(8, 12)),
-                'slug'=> $faker->slug(),
-                'excerpt'=>$faker->text(rand(255, 300)),
-                'body'=>$faker->paragraphs(rand(10, 15), true),
-                'image'=> rand(0,1) == 1 ? $image : NULL,
-                'created_at' => $date,
-                'updated_at' => $date,
-            ];
-        }
-        // dd($posts);
-        DB::table('posts')->insert($posts);
+         // generate 10 dummy posts data
+         $posts = [];
+         $faker = Factory::create();
+         $date = Carbon::create(2020, 10, 22, 11, 17, 00,'Europe/Paris');
+ 
+         for ($i = 1; $i <= 10; $i++)
+         {
+             $image = "Post_Image_" . rand(1, 5) . ".jpg";
+             $date->addDays(1);
+             $publishedDate = clone($date);
+             $createdDate   = clone($date);
+ 
+             $posts[] = [
+                 'author_id'    => rand(1, 3),
+                 'title'        => $faker->sentence(rand(8, 12)),
+                 'excerpt'      => $faker->text(rand(250, 300)),
+                 'body'         => $faker->paragraphs(rand(10, 15), true),
+                 'slug'         => $faker->slug(),
+                 'image'        => rand(0, 1) == 1 ? $image : NULL,
+                 'created_at'   => $createdDate,
+                 'updated_at'   => $createdDate,
+                 'published_at' => $i < 5 ? $publishedDate : ( rand(0, 1) == 0 ? NULL : $publishedDate->addDays(4) )
+             ];
+         }
+ 
+         DB::table('posts')->insert($posts);
+     }
 
 
     }
-}
+
