cf39335b064c59a80368e4a90c13db746f5d7510
simonet85 <christian.85@live.fr>
2020-10-26 16:57:49 +0100

    Add markdown support for the blog post

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -17,7 +17,7 @@
                 <div class="post-item-body">
                     <div class="padding-10">
                         <h2><a href="{{route('blog.show',['blog'=>$post->slug])}}">{{$post->title}}</a></h2>
-                        <p>{{$post->excerpt}}</p>
+                        {!! Markdown::convertToHtml( e($post->excerpt) ) !!}
                     </div>
 
                     <div class="post-meta padding-10 clearfix">
